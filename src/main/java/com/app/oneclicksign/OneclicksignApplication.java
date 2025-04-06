0c0f6af6cd59311aa4629390cdc324cadeefb02a
Rohit <paswanrohit0207@gmail.com>
2025-04-06 13:37:49 +0530

    Initial commit

---
+++ src/main/java/com/app/oneclicksign/OneclicksignApplication.java
@@ -0,0 +1,13 @@
+package com.app.oneclicksign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneclicksignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneclicksignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/oneclicksign/OneclicksignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.oneclicksign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneclicksignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
